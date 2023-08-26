a8583135b35b6523f597020ff7e7055bfaa1d26d
Ben Walker <ben.walker18@gmail.com>
2023-08-26 14:32:56 -0700

    Run main from gradle & take in arguments

--- README.md
+++ README.md
@@ -8,7 +8,12 @@ gradle build -x test
 
 # Test
 
-
 ```
 gradle test
+```
+
+# Run
+
+```
+gradle runMain --args="12:00 PM -1"
 ```
--- src/main/java/com/benefacto/timeadder/TimeAdder.java
+++ src/main/java/com/benefacto/timeadder/TimeAdder.java
@@ -2,12 +2,24 @@ package com.benefacto.timeadder;
 
 public class TimeAdder {
   public static void main(String[] args) {
-    System.out.println(addMinutes("12:00 PM", -1));
+    if (args.length < 3) {
+      System.out.println("Please provide the time string ([H]H:MM {AM|PM}) and minutes integer as arguments.");
+      return;
+    }
+    String timeString = String.format("%s %s", args[0], args[1]);
+    int minutes;
+    try {
+      minutes = Integer.parseInt(args[2]);
+    } catch (NumberFormatException e) {
+      System.out.println("Invalid number of minutes provided.");
+      return;
+    }
+    System.out.println(addMinutes(timeString, minutes));
   }
 
-  public static String addMinutes(String time, int minutes) {
-    Time timeManager = new Time(time);
-    timeManager.addMinutes(minutes);
-    return timeManager.toString();
+  public static String addMinutes(String timeStamp, int minutes) {
+    Time time = new Time(timeStamp);
+    time.addMinutes(minutes);
+    return time.toString();
   }
 }
